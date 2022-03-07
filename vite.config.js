7492d788a629b805bfbb1a902af341978720193b
h0gan1ee <lihaojun2003@outlook.com>
2022-03-07 10:25:14 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,20 @@
-import { defineConfig, } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver, } from 'unplugin-vue-components/resolvers';
-import { VitePWA, } from 'vite-plugin-pwa';
-import Compression from 'vite-plugin-compression';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Compression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VitePWA(),
     vue(),
     Compression(),
     AutoImport({
-      resolvers: [ElementPlusResolver(),],
-    },),
+      resolvers: [ElementPlusResolver()],
+    }),
     Components({
-      resolvers: [ElementPlusResolver(),],
-    },),
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
-},);
+})
