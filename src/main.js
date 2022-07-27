cbc92e8c623dcb08372921633f362613ea1237fb
h0gan1ee <lihaojun2003@outlook.com>
2022-07-27 10:54:23 +0800

    feat: add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router/dist/vue-router'
 
 import App from './App.vue'
+import MainPage from './pages/MainPage.vue'
+import RemoteControlPage from './pages/RemoteControlPage.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: MainPage },
+  { path: '/remote/:id', component: RemoteControlPage }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+createApp(App).use(router).mount('#app')
